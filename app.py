963b7d914d5da9d4770bcec5edfef11f7002b56e
Emanuel Pedroza <emanuelsalgado007@gmail.com>
2025-06-25 15:02:39 -0300

    Create file_uploader

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-import streamlit
+import streamlit as st
+st.title("Pizza or not pizza?")
+st.text("Without deep learning.")
+
+uploaded_file = st.file_uploader('Upload a food image.')
+if uploaded_file is not None:
+    print(uploaded_file)
